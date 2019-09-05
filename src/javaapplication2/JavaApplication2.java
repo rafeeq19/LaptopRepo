f8343beb2ca1be4b7f1c08b21aefd0530582a168
Raf <rafeeq.alhashemi@gmail.com>
2019-09-05 16:38:35 -0700

    Add new print statement

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -17,6 +17,7 @@ public class JavaApplication2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Git Demo 2019");
+System.out.println("Rafeeq new update 4:30pm");
     }
     
 }
